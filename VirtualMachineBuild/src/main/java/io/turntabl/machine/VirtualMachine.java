70bea8ac6205c6417aeba91e731afb382fd1d6ea
RhumeLeap <rhume.disi@turntabl.io>
2023-04-05 17:40:12 +0100

    Added NullPointerException to catch null value in hostname and requestor's name

--- VirtualMachineBuild/src/main/java/io/turntabl/machine/VirtualMachine.java
+++ VirtualMachineBuild/src/main/java/io/turntabl/machine/VirtualMachine.java
@@ -8,17 +8,24 @@ public abstract class VirtualMachine {
     protected int sizeOfRAM;
     protected int sizeOfHardDisk;
 
-    private static int MAXIMUM_VM_BUILT_DAILY = 999;
+    private static final int MAXIMUM_VM_BUILT_DAILY = 999;
 
     public VirtualMachine(String hostname, String requestorName) {
+        if (hostname == null || requestorName == null) {
+            throw new NullPointerException("Hostname and requestor name cannot be null");
+        }
         if (!checkHostnameFormat(hostname)) {
-            throw new WrongHostnameException("Hostname must be formated like 'host<date><build_ref_no>");
+            throw new WrongHostnameException("Hostname must be formatted like 'host<date><build_ref_no>");
         }
         this.hostname = hostname;
         this.requestorName = requestorName;
     }
 
     private boolean checkHostnameFormat(String name) {
+        int defaultHostnameLength = 15;
+        if (name.isEmpty() || name.length() < defaultHostnameLength) {
+            return false;
+        }
         String prefix = "host";
         String date = name.substring(4,12);
         String buildRequestNum = name.substring(12);
